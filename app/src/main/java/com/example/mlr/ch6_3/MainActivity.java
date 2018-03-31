0e248a02b4b5002700591fd179c466c5e0973a61
MLR-SUN <sun.ios06444@gmail.com>
2018-03-31 16:35:11 +0800

    1. 使用亂數取得num1 和 num2 的變數值

--- app/src/main/java/com/example/mlr/ch6_3/MainActivity.java
+++ app/src/main/java/com/example/mlr/ch6_3/MainActivity.java
@@ -8,14 +8,15 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int num1, num2; 
+    private int num1, num2; //用private宣告num1、num2為整數變數(private私有成員只能在自身類別中使用)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        num1 = (int) (Math.random()*12) + 1;
-        num2 = (int) (Math.random()*12) + 1;
+        //使用亂數取得數字
+        num1 = (int) (Math.random()*12) + 1; //Math.random()*12的值為0~12，加1後為1~13
+        num2 = (int) (Math.random()*12) + 1; //Math.random()*12的值為0~12，加1後為1~13
         Button btn1 = (Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(btn1listener);
         Button btn2 = (Button) findViewById(R.id.btn2);
