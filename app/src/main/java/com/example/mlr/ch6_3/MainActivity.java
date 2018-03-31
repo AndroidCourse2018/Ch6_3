9bfa71dc8c9d72de5aaa481a8bd7424493e1f1be
MLR-SUN <sun.ios06444@gmail.com>
2018-03-31 16:29:25 +0800

    兩個Button，點擊後產生亂數(可用來比大小)

--- app/src/main/java/com/example/mlr/ch6_3/MainActivity.java
+++ app/src/main/java/com/example/mlr/ch6_3/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.mlr.ch6_3;
 
+import android.Manifest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int num1, num2; 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        num1 = (int) (Math.random()*12) + 1;
+        num2 = (int) (Math.random()*12) + 1;
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(btn1listener);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(btn2listener);
     }
+
+    View.OnClickListener btn1listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, Integer.toString(num1) , Toast.LENGTH_SHORT).show();
+        }
+    };
+    View.OnClickListener btn2listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, Integer.toString(num2) , Toast.LENGTH_SHORT).show();
+        }
+    };
 }
